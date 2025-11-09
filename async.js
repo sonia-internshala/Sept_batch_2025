09bf36a9ef2277d615cdcad1be27f61518e46302
sonia-internshala <sonia.malik@internshala.com>
2025-11-09 12:59:09 +0530

    Add files via upload

---
+++ async.js
@@ -0,0 +1,52 @@
+// console.log("Open the shop!");
+// console.log("shop is opened for 10 seconds!");
+// console.log("shut the shop!");
+
+
+
+// console.log("Open the shop!");
+// //setTimeout -> 2 parameters -> 1 is callback function and 2 is timer(milliseconds);
+// // setTimeout(cb function, timer);
+// setTimeout(function(){
+//     console.log("The shop is open for 10 seconds.");
+// },10*1000)
+
+// console.log("shut the shop!");
+
+
+// console.log("One");
+
+// setTimeout(function(){
+//     console.log("Two");
+// },5000)
+
+// setTimeout(function(){
+//     console.log("Three");
+// },3000)
+// console.log("Four");
+
+//setInterval -> Web API -> 2 params -> cb fn | timer
+
+// let count=1;
+// let pentablet=setInterval(function(){
+//     count++;
+//     console.log("hello");
+//     if(count>5){
+//         clearInterval(pentablet);
+//     }
+// },2000);
+
+
+// let pentablet=setInterval(function(){
+//     console.log("hello");
+//         clearInterval(pentablet);
+// },2000);
+
+
+
+// let pentablet=setInterval(function(){
+//     console.log("hello");
+//     clearTimeout(pentablet);
+// },2000);
+
+
